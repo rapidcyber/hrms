34a53aa73d7da2a1d2b6b0752a2f13394b383f84
rapidcyber <jun.barcellano@realholmes.ph>
2025-06-10 15:24:29 +0800

    Fixed export attendance headings

--- app/Exports/AttendanceExport.php
+++ app/Exports/AttendanceExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Attendance;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class AttendanceExport implements FromCollection
+class AttendanceExport implements FromCollection,WithHeadings
 {
 
     protected $parameter; // This will hold your parameter
@@ -24,23 +25,30 @@ class AttendanceExport implements FromCollection
             ->select(
                 'attendances.employee_id',
                 \DB::raw("CONCAT(employees.first_name, ' ', employees.last_name) as employee_name"),
-                'attendances.in_1',
-                'attendances.out_1',
-                'attendances.in_2',
-                'attendances.out_2',
-                'attendances.in_3',
-                'attendances.out_3',
+                'attendances.date',
+                \DB::raw("TIME_FORMAT(attendances.in_1, '%H:%i:%s') as in_1"),
+                \DB::raw("TIME_FORMAT(attendances.out_1, '%H:%i:%s') as out_1"),
+                \DB::raw("TIME_FORMAT(attendances.in_2, '%H:%i:%s') as in_2"),
+                \DB::raw("TIME_FORMAT(attendances.out_2, '%H:%i:%s') as out_2"),
+                \DB::raw("TIME_FORMAT(attendances.in_3, '%H:%i:%s') as in_3"),
+                \DB::raw("TIME_FORMAT(attendances.out_3, '%H:%i:%s') as out_3"),
                 'attendances.hours_worked'
             )
             ->get();
 
         return $attendances;
     }
+    /**
+     * Define the headings for the export.
+     *
+     * @return array
+     */
     public function headings(): array
     {
         return [
             'Employee ID',
             'Employee Name',
+            'Date',
             'In 1',
             'Out 1',
             'In 2',
